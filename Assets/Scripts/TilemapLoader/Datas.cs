f30c56ee478da98033b87ebf4839308e812eaeee
AubeCiel <40396162+AubeCiel@users.noreply.github.com>
2020-05-13 16:14:40 +0900

    create Scripts
    Scene 생성 및 Script 생성 하고
    폴더분류

---
+++ Assets/Scripts/TilemapLoader/Datas.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/*
+                                데이터
+                                추후에 데이터 테이블 참고해서 수정
+ */
+
+[System.Serializable]
+public class TileData
+{
+    public string Name;
+    public Vector3 WorldPlace;
+    public Vector3Int LocalPlace;
+    public TilemapData BaseTilemap;
+}
+
+[System.Serializable]
+public class PrefabData
+{
+    public string Name;
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public Vector3 Scale;
+    public TilemapData BaseTileMap;
+}
+
+[System.Serializable]
+public class TilemapData
+{
+    public string Name;
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public Vector3 Scale;
+    public float AnimationFrameRate;
+    public Color Color;
+    public Vector3 TileAnchor;
+}
+
+[System.Serializable]
+public class MapData
+{
+    public List<TileData> Tiles;
+    public List<PrefabData> Prefabs;
+    public Vector3 PlayerStartPosition;
+    public Vector3 PlayerEndPosition;
+}
