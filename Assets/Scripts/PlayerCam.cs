014ec134cd86635023bb0b8871ffc3f6f11e9a55
Pistol97 <ehfdl75@gmail.com>
2020-06-05 04:51:39 +0900

    Edit camera bounds
    카메라 경계설정
    -카메라가 플레이어를 쫓아가지만 맵 밖을 비추지 않음

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -11,8 +11,8 @@ public class PlayerCam : MonoBehaviour
     private Vector3 offset;
 
     //맵 경계
-    //[SerializeField]
-    //private BoxCollider2D mapBound;
+    [SerializeField]
+    private BoxCollider2D mapBound;
 
     private Vector3 minBound;
     private Vector3 maxBound;
@@ -27,8 +27,8 @@ public class PlayerCam : MonoBehaviour
     {
         cam = GetComponent<Camera>();
 
-        //minBound = mapBound.bounds.min;
-        //maxBound = mapBound.bounds.max;
+        minBound = mapBound.bounds.min;
+        maxBound = mapBound.bounds.max;
 
         halfHeight = cam.orthographicSize;
         halfWidth = halfHeight * Screen.width / Screen.height;
@@ -41,9 +41,9 @@ public class PlayerCam : MonoBehaviour
         //카메라 위치 결정
         transform.position = new Vector3(target.position.x, target.position.y) + offset;
 
-        //float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
-        //float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+        float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+        float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
 
-        //this.transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+        this.transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
