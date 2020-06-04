03915fea4da60f554313513bed89f0ab2ca1064d
Pistol97 <ehfdl75@gmail.com>
2020-06-05 03:01:00 +0900

    Player camera sync
    플레이어 카메라 연동
    -최초 게임 실행시 플레이어에게 카메라가 연동되지 않던 문제 수정
    -임시 해결책이라 차후 수정 가능성

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
 {
-    [SerializeField]
+
     private Transform target;
 
-    public Vector3 offset;
+    [SerializeField]
+    private Vector3 offset;
 
     //맵 경계
-    [SerializeField]
-    private BoxCollider2D mapBound;
+    //[SerializeField]
+    //private BoxCollider2D mapBound;
 
     private Vector3 minBound;
     private Vector3 maxBound;
@@ -20,26 +21,29 @@ public class PlayerCam : MonoBehaviour
     private float halfWidth;
     private float halfHeight;
 
-    private Camera camera;
+    private Camera cam;
 
     private void Awake()
     {
-        camera = GetComponent<Camera>();
+        cam = GetComponent<Camera>();
 
-        minBound = mapBound.bounds.min;
-        maxBound = mapBound.bounds.max;
+        //minBound = mapBound.bounds.min;
+        //maxBound = mapBound.bounds.max;
 
-        halfHeight = camera.orthographicSize;
+        halfHeight = cam.orthographicSize;
         halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
+        target = GameObject.Find("Player(Clone)").transform;
+
+        //카메라 위치 결정
         transform.position = new Vector3(target.position.x, target.position.y) + offset;
 
-        float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
-        float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+        //float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+        //float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
 
-        this.transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+        //this.transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
---
+++ Assets/Tests/PlayerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerTest
+    {
+        [UnityTest]
+        public IEnumerator PlayerGroundedCheck()
+        {
+            Player player;
+
+            yield return null;
+        }
+    }
+}
