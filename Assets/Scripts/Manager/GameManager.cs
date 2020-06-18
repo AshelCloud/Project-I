79b707c88d56db0ef107c0d845a20e36fc75742e
Ashel <ashel.cloud@gmail.com>
2020-06-18 23:46:00 +0900

    modify GameManager
    필요없는 코드 제거

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,26 +7,4 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Text text;
-
-    private void Awake()
-    {
-    }
-
-    private void OnEnable()
-    {
-        Application.logMessageReceived += HandleLog;
-    }
-
-    private void OnDisable()
-    {
-        Application.logMessageReceived -= HandleLog;
-    }
-
-    private void HandleLog(string logMessage, string stackTrace, LogType type)
-    {
-        string log = DateTime.Now.ToString("MM-dd hh:mm:ss") + " -- " + logMessage;
-
-        text.text = log;
-    }
 }
