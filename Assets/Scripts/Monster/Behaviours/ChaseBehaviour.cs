0b0e6a07d19f0d31218cb10959290548c59a0f1d
Ashel <ashel.cloud@gmail.com>
2020-07-21 13:59:47 +0900

    fix chase error
    Chase때 몬스터가 움직이지 않는 버그 수정

--- Assets/Scripts/Monster/Behaviours/ChaseBehaviour.cs
+++ Assets/Scripts/Monster/Behaviours/ChaseBehaviour.cs
@@ -18,6 +18,8 @@ public class ChaseBehaviour : StateMachineBehaviour
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        _Rigidbody2D.velocity = new Vector2(_Rigidbody2D.velocity.normalized.x * speed * Time.deltaTime, _Rigidbody2D.velocity.y);
+        int dir = myMonster.transform.lossyScale.x < 0f ? -1 : 1;
+
+        _Rigidbody2D.velocity = new Vector2(dir * speed * Time.deltaTime, _Rigidbody2D.velocity.y);
     }
 }
