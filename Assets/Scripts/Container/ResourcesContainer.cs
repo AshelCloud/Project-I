10dc579a4a690e09ce836aa113a3b9a1ec23b336
Ashel <ashel.cloud@gmail.com>
2020-08-01 17:54:34 +0900

    add Loading System
    보완점이 필요한듯.
    일단 구현 완료

---
+++ Assets/Scripts/Container/ResourcesContainer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using TMPro;
+using UnityEngine;
+
+public class ResourcesContainer : MonoBehaviour
+{
+    private static Dictionary<string, Object> _cache;
+    private static Dictionary<string, Object> Cache
+    {
+        get
+        {
+            if(_cache == null)
+            {
+                _cache = new Dictionary<string, Object>();
+            }
+            return _cache;
+        }
+        set
+        {
+            _cache = value;
+        }
+    }
+
+    public static T Load<T>(string path) where T : Object
+    {
+        if(Cache.ContainsKey(path))
+        {
+            return (T)Cache[path];
+        }
+
+        Cache[path] = Resources.Load<T>(path);
+
+        return (T)Cache[path];
+    }
+
+    //public static GameObject Instance(string key)
+    //{
+    //    GameObject go = Instantiate(Cache[key]);
+
+    //    return go;
+    //}
+
+    //public static GameObject Instance(string key, string _name)
+    //{
+    //    GameObject go = Instantiate(Cache[key]);
+    //    go.name = _name;
+
+    //    return go;
+    //}
+
+    //public static GameObject Instance(string key, Transform parent)
+    //{
+    //    GameObject go = Instantiate(Cache[key], parent);
+
+    //    return go;
+    //}
+
+    //public static GameObject Instance(string key, string _name, Vector3 pos)
+    //{
+    //    GameObject go = Instantiate(Cache[key], pos, Quaternion.identity);
+    //    go.name = _name;
+
+    //    return go;
+    //}
+
+    //public static GameObject Instance(string key, string _name, Vector3 pos, Quaternion rot)
+    //{
+    //    GameObject go = Instantiate(Cache[key], pos, rot);
+    //    go.name = _name;
+
+    //    return go;
+    //}
+}
--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,15 +1,45 @@
 ﻿using System.Collections;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Loader : MonoBehaviour
 {
-    private void Start()
+    public Image barImage;
+    public Text descriptionText;
+
+    private IEnumerator Start()
     {
+        descriptionText.text = "맵 데이터 로딩중...";
+
+        yield return null;
+
         AssetBundle result = AssetBundleContainer.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "jsons"));
         if(result == null)
         {
             Log.PrintError("Failed to Load Jsons");
         }
+
+        yield return null;
+
+        descriptionText.text = "맵 타일 로딩중...";
+
+        yield return null;
+
+        result = AssetBundleContainer.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "tiles"));
+        if(result == null)
+        {
+            Log.PrintError("Failed to Load Tiles");
+        }
+
+        yield return null;
+
+        descriptionText.text = "로딩 완료";
+
+        yield return new WaitForSeconds(0.5f);
+
+        //TODO: Scene 이동 바꾸기
+        SceneManager.LoadSceneAsync("ProtoType");
     }
 }
