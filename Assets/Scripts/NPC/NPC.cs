5a9c3c5d7a1127464368482de2f8874cb0a97f92
Ashel <ashel.cloud@gmail.com>
2020-08-18 14:54:25 +0900

    WIP: Add NPC Table

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    void Start()
+    {
+        var text = AssetBundleContainer.LoadAsset<TextAsset>("jsons", "NPCTable");
+
+        Log.Print(text.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
