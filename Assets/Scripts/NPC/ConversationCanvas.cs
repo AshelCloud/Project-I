ab1089325b676f99ea50ebf7952b67237b31500b
Ashel <ashel.cloud@gmail.com>
2020-08-19 21:36:06 +0900

    WIP: add canvasManager

---
+++ Assets/Scripts/NPC/ConversationCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConversationCanvas : MonoBehaviour
+{
+    public GameObject overlay;
+
+    public void OpenOverlay()
+    {
+        Log.Print("Open");
+    }
+}
--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    [System.Serializable]
+    private class NPCDataTable
+    {
+        public string NpcName;
+        public string PrefabName;
+        public string Text;
+        public string RandomText;
+    }
+
+    public string ID = "1";
+
+    public string[] Texts;
+
     void Start()
     {
         var text = AssetBundleContainer.LoadAsset<TextAsset>("jsons", "NPCTable");
+        var data = JsonManager.LoadJson<Serialization<string, NPCDataTable>>(text).ToDictionary();
 
-        Log.Print(text.text);
-    }
+        Texts = data[ID].Text.Split('\n');
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        CanvasManager.Instance.ConversationCanvas.enabled = true;
     }
 }
