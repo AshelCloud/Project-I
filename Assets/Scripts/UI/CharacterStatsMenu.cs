55d375fcc6dbd1417d01fb1d06dab1c8940dde52
Pistol97 <ehfdl75@gmail.com>
2020-09-02 01:37:59 +0900

    Edit CharacterStats Menu in PauseMenu

---
+++ Assets/Scripts/UI/CharacterStatsMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStatsMenu : MonoBehaviour
+{
+    private Player player = null;
+
+    private GameObject menu = null;
+
+    private Text value_HP;
+    private Text value_Power;
+    private Text value_Defence;
+
+    private void Start()
+    {
+        StartCoroutine(SearchObject());
+    }
+
+    void LateUpdate()
+    {
+        value_HP.text = player.HP.ToString();
+        value_Power.text = player.OffensePower.ToString();
+        value_Defence.text = player.defense.ToString();
+    }
+
+    private IEnumerator SearchObject()
+    {
+        yield return new WaitForSeconds(1f);
+
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        menu = GameObject.Find("Menu_CharacterStats");
+
+        value_HP = menu.transform.GetChild(1).transform.GetChild(0).GetComponent<Text>();
+        value_Power = menu.transform.GetChild(2).transform.GetChild(0).GetComponent<Text>();
+        value_Defence = menu.transform.GetChild(3).transform.GetChild(0).GetComponent<Text>();
+    }
+
+}
