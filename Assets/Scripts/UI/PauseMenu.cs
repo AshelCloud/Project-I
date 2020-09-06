7361b820086b2701780b1bae03a37f63c0d39e29
Pistol97 <ehfdl75@gmail.com>
2020-09-06 16:20:51 +0900

    Update pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -5,11 +5,44 @@ public class PauseMenu : MonoBehaviour
     [SerializeField]
     private CanvasGroup group_1, group_2, group_3 = null;
 
+    private bool isOpened = false;
+
     private void Start()
     {
         group_1.alpha = 1f;
         group_2.alpha = 0f;
         group_3.alpha = 0f;
+    }
+
+    private void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            isOpened = true;
+        }
+
+        else if (isOpened)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                isOpened = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SelectStatisticsMenu();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                SelectInventoryMenu();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                SelectSettingMenu();
+            }
+        }
     }
 
     public void SelectStatisticsMenu()
