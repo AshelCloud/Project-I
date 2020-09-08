fd66a5d1cc7019290e529feb7aa072ddd5d96ada
AshelCloud <ashel.cloud@gmail.com>
2020-09-09 00:25:01 +0900

    change UI
    플레이어 스탯창 UI 변경

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -1,13 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
-    private UnityEngine.UI.Image healthAmount = null;
-
-    private void Awake()
-    {
-        healthAmount = transform.GetChild(1).GetComponent<UnityEngine.UI.Image>();
-    }
+    public Image healthAmount = null;
+    public Image staminaAmount = null;
 
     private void LateUpdate()
     {
